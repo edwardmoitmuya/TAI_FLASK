6e8e7a14d584bbe5fa1af8918ade8735b35c01a8
edwardmoitmuya <edwardmoitmuya@gmail.com>
2026-02-25 08:41:57 +0300

    initial flask app

--- taisokogarden.sql
+++ taisokogarden.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Feb 18, 2026 at 09:17 AM
+-- Generation Time: Feb 25, 2026 at 06:19 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.1.25
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `kindisokogarden`
+-- Database: `taisokogarden`
 --
 
 -- --------------------------------------------------------
@@ -31,9 +31,21 @@ CREATE TABLE `product_details` (
   `product_id` int(11) NOT NULL,
   `product_name` varchar(255) NOT NULL,
   `product_description` text NOT NULL,
-  `produst_cost` int(11) NOT NULL,
+  `product_cost` int(11) NOT NULL,
   `product_photo` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `product_details`
+--
+
+INSERT INTO `product_details` (`product_id`, `product_name`, `product_description`, `product_cost`, `product_photo`) VALUES
+(1, 'cake', 'sweet cake with several colours candles on top', 8800, 'cimabue-cake-916253_1920.jpg'),
+(2, 'AirMax', 'white blue stripe size 43', 5800, 'airmax_white_blue_43.jpg'),
+(3, 'Toyota', 'high quality with long lifespan', 2500000, 'shoe2.jpeg'),
+(4, 'shoes', 'high quality with long lifespan', 2500, 'shoe2.jpeg'),
+(12, 'bed', 'sponchy nice bed', 50000, 'home-3678961_1920.jpg'),
+(13, 'cake', 'sweet cake with several colours candles on top', 8800, 'cimabue-cake-916253_1920.jpg');
 
 -- --------------------------------------------------------
 
@@ -54,9 +66,12 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `username`, `password`, `email`, `phone`) VALUES
-(1, 'Notch', '87654321', 'notchtheg@gmail.com', '0794563287'),
-(2, 'JohnD', '73026364', 'johndoe@gmail.com', '0795423541'),
-(3, 'BikerNairobi', '3287Secur3', 'bk254@gmail.com', '0784816548');
+(1, 'John Natede', '12345', 'join@gmailnatede', '0712345678'),
+(2, 'Patrick Pire', '987654', 'pire@gmailmuya', '0799754678'),
+(3, 'Jesca Ekuwam', '56369915', 'monteck@Edu', '0112486532'),
+(4, 'Paul Adekenyo', '987654', 'Paul@Kanyaite', '0796689143'),
+(5, 'Ambrose Etabo', '5636', 'etabo@amborose', '0756277404'),
+(6, 'Ambrose Etabo', '5636', 'etabo@amborose', '0756277404');
 
 --
 -- Indexes for dumped tables
@@ -82,13 +97,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `product_details`
 --
 ALTER TABLE `product_details`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
